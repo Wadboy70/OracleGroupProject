08f092e327a3375e3454923f174fd652e0eaf902
AllieWu <44093216+AllieWu@users.noreply.github.com>
2021-04-18 16:43:23 -0400

    added labels

--- src/oracle.js
+++ src/oracle.js
@@ -62,48 +62,103 @@ export const queries = [
       "Aspiring artists and music hobbyists can observe that there is an increasing trend in explicit songs. It is noteably more acceptable and common for cursing to be present in songs.",
   },
   {
-      title: "#2 Average artist career timespan overtime",
-      query: (startValue, endValue) => `WITH careerLength AS ( SELECT  artist_id,  MIN(s.song_year) AS startYear,  MAX(s.song_year) AS endYear,  MAX(s.song_year) - MIN(s.song_year) + 1 AS careerLength FROM relationship_song_artist r JOIN song s ON r.song_id = s.song_id GROUP BY r.artist_id)SELECT startYear, AVG(careerLength)FROM careerLength WHERE startYear > ${startValue || 1964} AND startYear < ${endValue || 2021} GROUP BY startYear ORDER BY startYear DESC`,
-      func: data => {
-          let rows = data?.val?.map(row => ({primary: row.STARTYEAR, secondary: row["AVG(CAREERLENGTH)"]})).reverse() || [];
-          return(
-              [
-                  {
-                      label: "yuh",
-                      data: rows
-                  }
-              ]
-          )
+    title: "#2 Average artist career timespan overtime",
+    query: (startValue, endValue) =>
+      `WITH careerLength AS ( SELECT  artist_id,  MIN(s.song_year) AS startYear,  MAX(s.song_year) AS endYear,  MAX(s.song_year) - MIN(s.song_year) + 1 AS careerLength FROM relationship_song_artist r JOIN song s ON r.song_id = s.song_id GROUP BY r.artist_id)SELECT startYear, AVG(careerLength)FROM careerLength WHERE startYear > ${
+        startValue || 1964
+      } AND startYear < ${
+        endValue || 2021
+      } GROUP BY startYear ORDER BY startYear DESC`,
+    func: (data) => {
+      let rows =
+        data?.val
+          ?.map((row) => ({
+            primary: row.STARTYEAR,
+            secondary: row["AVG(CAREERLENGTH)"],
+          }))
+          .reverse() || [];
+      return [
+        {
+          label: "yuh",
+          data: rows,
+        },
+      ];
+    },
+    axes_: [
+      {
+        primary: true,
+        type: "ordinal",
+        position: "bottom",
       },
-      axes_: [
-          { primary: true, type: 'ordinal', position: 'bottom', maxLabelRotation: 90 },
-          { type: 'linear', position: 'left'},
-      ],
-      series_: {type: "line"}
-
+      { type: "linear", position: "left" },
+    ],
+    series_: { type: "line" },
+    options_: {
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              autoSkip: false,
+              maxRotation: 90,
+              minRotation: 90,
+            },
+          },
+        ],
+      },
+    },
+    desc:
+      "There is a clear decrease in artist career span over time. Meaning, on average, new artists are only active for a year or so.",
   },
   {
-      title: "#3 How has the average number of tracks per album changed over time",
-      query: (startValue, endValue) => `WITH dates AS ( SELECT r.album_id,r.track_number,SUBSTR(s.release_date, 0, LENGTH(s.release_date) - 3) AS trimmedDate FROM relationship_song_album r JOIN song s ON s.song_id = r.song_id  ), albumStats AS ( SELECT album_id,MAX(track_number) AS NumTracks,MAX(trimmedDate) AS AlbumDate FROM dates GROUP BY album_id  )  SELECT AlbumDate, AVG(NumTracks)  FROM albumStats  WHERE SUBSTR(AlbumDate, 0, LENGTH(AlbumDate) - 3)> ${startValue || 1925} AND SUBSTR(AlbumDate, 0, LENGTH(AlbumDate) - 3) < ${endValue || 2021}  GROUP BY AlbumDate  ORDER BY AlbumDate desc`,
-      func: data => {
-          let rows = data?.val?.map(row => ({primary: row.ALBUMDATE, secondary: row["AVG(NUMTRACKS)"]})).reverse() || [];
-          console.log(data)
+    title:
+      "#3 How has the average number of tracks per album changed over time",
+    query: (startValue, endValue) =>
+      `WITH dates AS ( SELECT r.album_id,r.track_number,SUBSTR(s.release_date, 0, LENGTH(s.release_date) - 3) AS trimmedDate FROM relationship_song_album r JOIN song s ON s.song_id = r.song_id  ), albumStats AS ( SELECT album_id,MAX(track_number) AS NumTracks,MAX(trimmedDate) AS AlbumDate FROM dates GROUP BY album_id  )  SELECT SUBSTR(AlbumDate, 0, LENGTH(AlbumDate) - 3), AVG(NumTracks)  FROM albumStats  WHERE SUBSTR(AlbumDate, 0, LENGTH(AlbumDate) - 3)> ${
+        startValue || 1925
+      } AND SUBSTR(AlbumDate, 0, LENGTH(AlbumDate) - 3) < ${
+        endValue || 2021
+      }  GROUP BY AlbumDate  ORDER BY AlbumDate desc`,
+    func: (data) => {
+      let rows =
+        data?.val
+          ?.map((row) => ({
+            primary: row.ALBUMDATE,
+            secondary: row["AVG(NUMTRACKS)"],
+          }))
+          .reverse() || [];
+      console.log(data);
 
-          return(
-              [
-                  {
-                      label: "yuh",
-                      data: rows
-                  }
-              ]
-          )
+      return [
+        {
+          label: "yuh",
+          data: rows,
+        },
+      ];
+    },
+    axes_: [
+      {
+        primary: true,
+        type: "ordinal",
+        position: "bottom",
       },
-      axes_: [
-          { primary: true, type: 'ordinal', position: 'bottom', maxLabelRotation: 90 },
-          { type: 'linear', position: 'left'},
-      ],
-      series_: {type: "line"}
-
+      { type: "linear", position: "left" },
+    ],
+    series_: { type: "line" },
+    options_: {
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              autoSkip: false,
+              maxRotation: 90,
+              minRotation: 90,
+            },
+          },
+        ],
+      },
+    },
+    desc:
+      "The number of songs in an alabum has smoothly flucutated over time. In the past few decades, it has been slowly decreasing, stabilizing to about 5 songs per album.",
   },
   {
     title: "#5 Average loudness for top 200 songs and other songs over time",
